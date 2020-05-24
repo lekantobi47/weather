88b6836fdb17bc855a73f097a8f8f4190f9edba7
Akintilebo Olamilekan <lekantobi47@gmail.com>
2020-05-24 12:32:04 +0100

    Modify app.js
    * Set view engine for the app
    * Make the app send static files
    * Use body-parser middleware
    * Make the app render index.ejs file

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+// Set view engine
+app.set('view engine', 'ejs');
+
+// Make the app send static files in public folder
+app.use(express.static('public'));
+
+// Use body-parser middleware to get post requests data
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.render('index');
 });
 
 app.listen(port, () => {
